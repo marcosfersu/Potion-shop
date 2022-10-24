75996ca2b666c33972d00e381968b31ec3d34bf4
marcos.fersu <marcos.fersu@gmail.com>
2022-10-24 10:27:33 -0300

    marcos.fesu
    add new light en envireomnet
    adjust expoourse light

--- Experience/Word/Environment.js
+++ Experience/Word/Environment.js
@@ -2,35 +2,42 @@ import * as THREE from "three";
 import Experience from "../Experience";
 
 export default class Environment {
-  constructor() {
-    this.experience = new Experience();
-    this.scene = this.experience.scene;
-    this.resources = this.experience.resources;
+	constructor() {
+		this.experience = new Experience();
+		this.scene = this.experience.scene;
+		this.resources = this.experience.resources;
 
-    this.setSunLight();
-  }
+		this.setSunLight();
+	}
 
-  setSunLight() {
-    this.sunLight = new THREE.DirectionalLight("#fff", 1.5);
-    this.sunLight.castShadow = true;
-    this.sunLight.shadow.camera.far = 20;
-    this.sunLight.shadow.mapSize.set(2048, 2048);
-    this.sunLight.shadow.normalBias = 0.05;
-    this.sunLight.position.set(2, 1.4, 1);
-    this.scene.add(this.sunLight);
+	setSunLight() {
+		this.sunLight = new THREE.DirectionalLight("#fff", 1.5);
+		this.sunLight2 = new THREE.DirectionalLight("#fff", 0.6);
+		this.sunLight.castShadow = true;
+		this.sunLight2.castShadow = true;
+		this.sunLight.shadow.camera.far = 20;
+		this.sunLight2.shadow.camera.far = 20;
+		this.sunLight.shadow.mapSize.set(2048, 2048);
+		this.sunLight2.shadow.mapSize.set(2048, 2048);
+		this.sunLight.shadow.normalBias = 0.05;
+		this.sunLight2.shadow.normalBias = 0.05;
+		this.sunLight.position.set(2, 1.4, 1);
+		this.sunLight2.position.set(-3, 2, 1.4);
+		this.scene.add(this.sunLight);
+		this.scene.add(this.sunLight2);
 
-    this.ambientLight = new THREE.AmbientLight("#fff", 1);
-    this.scene.add(this.ambientLight);
+		this.ambientLight = new THREE.AmbientLight("#fff", 1.2);
+		this.scene.add(this.ambientLight);
 
-    this.sphereSize = 1;
-    this.pointLightHelper = new THREE.PointLightHelper(
-      this.sunLight,
-      this.sphereSize
-    );
-    //this.scene.add(this.pointLightHelper);
-  }
+		this.sphereSize = 1;
+		this.pointLightHelper = new THREE.PointLightHelper(
+			this.sunLight2,
+			this.sphereSize
+		);
+		this.scene.add(this.pointLightHelper);
+	}
 
-  resize() {}
+	resize() {}
 
-  update() {}
+	update() {}
 }
