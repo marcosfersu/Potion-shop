ca91e386d63276ea98b6a0fd156f7f4f1eb4a863
Marcos Fernández <marcos.fersu@gmail.com>
2022-10-27 11:04:43 -0300

    marcos.fersu
    add element index

--- Experience/Camera.js
+++ Experience/Camera.js
@@ -3,85 +3,85 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import Experience from "./Experience";
 
 export default class Camera {
-	constructor() {
-		this.experience = new Experience();
-		this.sizes = this.experience.sizes;
-		this.scene = this.experience.scene;
-		this.canvas = this.experience.canvas;
+  constructor() {
+    this.experience = new Experience();
+    this.sizes = this.experience.sizes;
+    this.scene = this.experience.scene;
+    this.canvas = this.experience.canvas;
 
-		this.createPerspectiveCamera();
-		this.createOrthographicCamera();
-		this.setOrbitControls();
-	}
+    this.createPerspectiveCamera();
+    this.createOrthographicCamera();
+    this.setOrbitControls();
+  }
 
-	createPerspectiveCamera() {
-		this.perspectiveCamera = new THREE.PerspectiveCamera(
-			35,
-			this.sizes.aspect,
-			0.1,
-			1000
-		);
-		this.scene.add(this.perspectiveCamera);
-		this.perspectiveCamera.position.x = 21;
-		this.perspectiveCamera.position.y = 16;
-		this.perspectiveCamera.position.z = -15;
-	}
-	createOrthographicCamera() {
-		this.orthographicCamera = new THREE.OrthographicCamera(
-			(-this.sizes.aspect * this.sizes.frustrum) / 2,
-			(this.sizes.aspect * this.sizes.frustrum) / 2,
-			this.sizes.frustrum / 2,
-			-this.sizes.frustrum / 2,
-			-50,
-			50
-		);
+  createPerspectiveCamera() {
+    this.perspectiveCamera = new THREE.PerspectiveCamera(
+      35,
+      this.sizes.aspect,
+      0.1,
+      1000
+    );
+    this.scene.add(this.perspectiveCamera);
+    this.perspectiveCamera.position.x = 21;
+    this.perspectiveCamera.position.y = 16;
+    this.perspectiveCamera.position.z = -15;
+  }
+  createOrthographicCamera() {
+    this.orthographicCamera = new THREE.OrthographicCamera(
+      (-this.sizes.aspect * this.sizes.frustrum) / 2,
+      (this.sizes.aspect * this.sizes.frustrum) / 2,
+      this.sizes.frustrum / 2,
+      -this.sizes.frustrum / 2,
+      -50,
+      50
+    );
 
-		this.orthographicCamera.position.y = 3.8;
-		this.orthographicCamera.position.z = 5;
-		this.orthographicCamera.rotation.x = -Math.PI / 6.2;
+    this.orthographicCamera.position.y = 3.8;
+    this.orthographicCamera.position.z = 5;
+    this.orthographicCamera.rotation.x = -Math.PI / 6.2;
 
-		this.scene.add(this.orthographicCamera);
+    this.scene.add(this.orthographicCamera);
 
-		//this.helper = new THREE.CameraHelper(this.orthographicCamera);
-		//this.scene.add(this.helper);
+    //this.helper = new THREE.CameraHelper(this.orthographicCamera);
+    //this.scene.add(this.helper);
 
-		const size = 10;
-		const divisions = 10;
+    const size = 10;
+    const divisions = 10;
 
-		//const gridHelper = new THREE.GridHelper(size, divisions);
-		//this.scene.add(gridHelper);
+    //const gridHelper = new THREE.GridHelper(size, divisions);
+    //this.scene.add(gridHelper);
 
-		const axesHelper = new THREE.AxesHelper(10);
-		this.scene.add(axesHelper);
-	}
+    //const axesHelper = new THREE.AxesHelper(10);
+    //this.scene.add(axesHelper);
+  }
 
-	setOrbitControls() {
-		this.controls = new OrbitControls(this.perspectiveCamera, this.canvas);
-		this.controls.enableDamping = true;
-		this.controls.enableZoom = true;
-	}
+  setOrbitControls() {
+    this.controls = new OrbitControls(this.perspectiveCamera, this.canvas);
+    this.controls.enableDamping = true;
+    this.controls.enableZoom = true;
+  }
 
-	resize() {
-		// updation Perspective Camra on Resize
-		this.perspectiveCamera.aspect = this.sizes.aspect;
-		this.perspectiveCamera.updateProjectionMatrix();
+  resize() {
+    // updation Perspective Camra on Resize
+    this.perspectiveCamera.aspect = this.sizes.aspect;
+    this.perspectiveCamera.updateProjectionMatrix();
 
-		// updation Orthographic Camra on Resize
-		this.orthographicCamera.left =
-			(-this.sizes.aspect * this.sizes.frustrum) / 2;
-		this.orthographicCamera.right =
-			(this.sizes.aspect * this.sizes.frustrum) / 2;
-		this.orthographicCamera.top = this.seizes.frustrum / 2;
-		this.orthographicCamera.bottom = this.seizes.frustrum / 2;
-		this.orthographicCamera.updateProjectionMatrix();
-	}
+    // updation Orthographic Camra on Resize
+    this.orthographicCamera.left =
+      (-this.sizes.aspect * this.sizes.frustrum) / 2;
+    this.orthographicCamera.right =
+      (this.sizes.aspect * this.sizes.frustrum) / 2;
+    this.orthographicCamera.top = this.seizes.frustrum / 2;
+    this.orthographicCamera.bottom = this.seizes.frustrum / 2;
+    this.orthographicCamera.updateProjectionMatrix();
+  }
 
-	update() {
-		this.controls.update();
-		//console.log(this.perspectiveCamera.position);
-		//this.helper.matrixWorldNeedsUpdate = true;
-		//this.helper.update();
-		//this.helper.position.copy(this.orthographicCamera.position);
-		//this.helper.rotation.copy(this.orthographicCamera.rotation);
-	}
+  update() {
+    this.controls.update();
+    //console.log(this.perspectiveCamera.position);
+    //this.helper.matrixWorldNeedsUpdate = true;
+    //this.helper.update();
+    //this.helper.position.copy(this.orthographicCamera.position);
+    //this.helper.rotation.copy(this.orthographicCamera.rotation);
+  }
 }
--- Experience/Word/Floor.js
+++ Experience/Word/Floor.js
@@ -2,27 +2,27 @@ import * as THREE from "three";
 import Experience from "../Experience";
 
 export default class Floor {
-	constructor() {
-		this.experience = new Experience();
-		this.scene = this.experience.scene;
+  constructor() {
+    this.experience = new Experience();
+    this.scene = this.experience.scene;
 
-		this.setFloor();
-	}
+    this.setFloor();
+  }
 
-	setFloor() {
-		this.geometry = new THREE.PlaneGeometry(100, 100);
-		this.material = new THREE.MeshStandardMaterial({
-			color: 0xffffff,
-			side: THREE.DoubleSide,
-		});
-		this.plane = new THREE.Mesh(this.geometry, this.material);
-		this.scene.add(this.plane);
-		this.plane.rotation.x = Math.PI / 2;
-		this.plane.position.y = -0.2;
-		this.plane.receiveShadow = true;
-	}
+  setFloor() {
+    this.geometry = new THREE.PlaneGeometry(100, 100);
+    this.material = new THREE.MeshStandardMaterial({
+      color: 0xbdf4ff,
+      side: THREE.DoubleSide,
+    });
+    this.plane = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.plane);
+    this.plane.rotation.x = Math.PI / 2;
+    this.plane.position.y = -0.2;
+    this.plane.receiveShadow = true;
+  }
 
-	resize() {}
+  resize() {}
 
-	update() {}
+  update() {}
 }
