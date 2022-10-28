00b7598358958042f14f3a2a1f83f20a581374ba
marcos.fersu <marcos.fersu@gmail.com>
2022-10-28 17:02:20 -0300

    fix center

--- Experience/Word/Controls.js
+++ Experience/Word/Controls.js
@@ -20,11 +20,11 @@ export default class Controls {
 		this.timeline = new GSAP.timeline();
 		this.timeline.to(this.room.position, {
 			x: () => {
-				return this.sizes.width * 0.0014;
+				return this.sizes.width * 0.0012;
 			},
 			scrollTrigger: {
 				trigger: ".first-move",
-				markers: true,
+				//markers: true,
 				start: "top top",
 				end: "bottom bottom",
 				scrub: 0.6,
