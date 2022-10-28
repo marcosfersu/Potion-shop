6b3a527108ede55174c1e6f76d02738f4ca95604
marcos.fersu <marcos.fersu@gmail.com>
2022-10-28 15:29:58 -0300

    idk

--- Experience/Renderer.js
+++ Experience/Renderer.js
@@ -2,43 +2,43 @@ import * as THREE from "three";
 import Experience from "./Experience";
 
 export default class Renderer {
-  constructor() {
-    this.experience = new Experience();
-    this.sizes = this.experience.sizes;
-    this.scene = this.experience.scene;
-    this.canvas = this.experience.canvas;
-    this.camera = this.experience.camera;
+	constructor() {
+		this.experience = new Experience();
+		this.sizes = this.experience.sizes;
+		this.scene = this.experience.scene;
+		this.canvas = this.experience.canvas;
+		this.camera = this.experience.camera;
 
-    // console.log(this.camera, this.camera.perspectiveCamera);
+		// console.log(this.camera, this.camera.perspectiveCamera);
 
-    this.setRenderer();
-  }
+		this.setRenderer();
+	}
 
-  setRenderer() {
-    this.renderer = new THREE.WebGLRenderer({
-      canvas: this.canvas,
-      antialias: true,
-    });
+	setRenderer() {
+		this.renderer = new THREE.WebGLRenderer({
+			canvas: this.canvas,
+			antialias: true,
+		});
 
-    this.renderer.physicallyCorrectLights = true;
-    this.renderer.outputEncoding = THREE.sRGBEncoding;
-    this.renderer.toneMapping = THREE.CineonToneMapping;
-    this.renderer.toneMappingExposure = 1.5;
-    this.renderer.shadowMap.enabled = true;
-    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-    this.renderer.setSize(this.sizes.width, this.sizes.height);
-    this.renderer.setPixelRatio(this.sizes.pixelRatio);
-  }
+		this.renderer.physicallyCorrectLights = true;
+		this.renderer.outputEncoding = THREE.sRGBEncoding;
+		this.renderer.toneMapping = THREE.CineonToneMapping;
+		this.renderer.toneMappingExposure = 1.5;
+		this.renderer.shadowMap.enabled = true;
+		this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+		this.renderer.setSize(this.sizes.width, this.sizes.height);
+		this.renderer.setPixelRatio(this.sizes.pixelRatio);
+	}
 
-  resize() {
-    this.renderer.setSize(this.sizes.width, this.sizes.height);
-    this.renderer.setPixelRatio(this.sizes.pixelRatio);
-  }
+	resize() {
+		this.renderer.setSize(this.sizes.width, this.sizes.height);
+		this.renderer.setPixelRatio(this.sizes.pixelRatio);
+	}
 
-  update() {
-    //this.renderer.setViewport(0, 0, this.sizes.width, this.sizes.height);
-    this.renderer.render(this.scene, this.camera.orthographicCamera);
-    /*
+	update() {
+		//this.renderer.setViewport(0, 0, this.sizes.width, this.sizes.height);
+		this.renderer.render(this.scene, this.camera.orthographicCamera);
+		/*
 		//Second Screen
 		this.renderer.setScissorTest(true);
 		this.renderer.setViewport(
@@ -59,5 +59,5 @@ export default class Renderer {
 
 		this.renderer.setScissorTest(false);
     */
-  }
+	}
 }
